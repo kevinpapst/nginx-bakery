a3fd30ad108cffa6c4bb37bd214ac2efffb232c6
Kevin Papst <kpapst@gmx.net>
2013-12-17 11:13:41 +0100

    new cookbooks

---
+++ cookbooks/php-fpm.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Cookbook for a phph fastcgi site.
+ *
+ * @param redirect
+ * @param server_name
+ * @param root
+ * @param include
+ */
+return array(
+    array(
+        'recipe'    => 'http-redirect_server',
+        'config'    => array(
+            'server_name'   => '%redirect%',
+            'target'        => '%server_name%'
+        ),
+    ),
+    array(
+        'recipe'    => 'https-redirect_server',
+        'config'    => array(
+            'server_name'   => '%redirect%',
+            'target'        => '%server_name%'
+        ),
+    ),
+    array(
+        'recipe'    => array('http-default', 'https-default'),
+        'config'    => array(
+            'server_name'       => '%server_name%',
+            'root'              => '%root%',
+            'index'             => 'index.php',
+            'includes'          => array(
+                '%include%', 'favicon', 'php-fpm', 'cache-assets', 'htaccess'
+            )
+        ),
+    ),
+);
